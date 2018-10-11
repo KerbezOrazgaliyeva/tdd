a9cee9e126cd46743356d214f81dad0c0a99ee53
KerbezOrazgaliyeva <160107140@stu.sdu.edu.kz>
2018-10-11 22:01:25 +0600

    l

---
+++ tests/find_max_test.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class FindMaxTest(TestCase):
+    def test_get_max(self):
+        pass
+
+    def test_get_max_without_arguments(self):
+        pass
+
+    def test_get_max_with_one_argument(self):
+        pass
+
+    def test_get_max_with_many_arguments(self):
+        pass
+
+    def test_get_max_with_one_or_more_arguments(self):
+        pass
+
+    def test_get_max_bounded(self):
+        pass
+
+    def test_make_max(self):
+        pass
