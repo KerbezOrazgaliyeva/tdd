d42a9c33f2a468998f107a15da36d0fa9233a18c
KerbezOrazgaliyeva <160107140@stu.sdu.edu.kz>
2018-10-12 00:27:32 +0600

    done 100%

--- answers/find_min.py
+++ answers/find_min.py
@@ -1,16 +1,15 @@
-
-
 def get_min(a, b):
     """
         return min number among a and b
     """
     return a if a < b else b
 
+
 def get_min_without_arguments():
     """
         raise TypeError exception with message
     """
-    raise TypeError('no arguments')
+    raise TypeError('missed arguments')
 
 
 def get_min_with_one_argument(x):
@@ -22,34 +21,36 @@ def get_min_with_one_argument(x):
 
 def get_min_with_many_arguments(*args):
     """
-	    return smallest number among args
+        return smallest number among args
     """
-    r = 100000000
-    for ar in args:
-        if ar < r:
-            r = ar
-    return r
+    result = float('inf')
+    for arg in args:
+        if arg < result:
+            result = arg
+    return result
 
 
 def get_min_with_one_or_more_arguments(first, *args):
     """
         return smallest number among first + args
     """
-    for ar in args:
-        if ar < first:
-            first = ar
-    return first
+    result = float('inf')
+    for arg in (first,) + args:
+        if arg < result:
+            result = arg
+    return result
 
 
 def get_min_bounded(*args, low, high):
     """
         return smallest number among args bounded by low & high
     """
-    r = 100000000
-    for ar int args:
-        if ar < r and low<ar<high:
-            r = ar
-    return r
+    res = float('inf')
+    for arg in args:
+        if arg < res and low < arg < high:
+            res = arg
+    return res
+
 
 def make_min(*, low, high):
     """
@@ -58,11 +59,13 @@ def make_min(*, low, high):
         smallest number is lower than the 'low' which given as required
         argument the inner function has to return it.
     """
+
     def inner(first, *args):
-        r = first
-        for a in args:
-            if r > a:
-                r = a
-        return r
+        result = float('inf')
+
+        for arg in (first,) + args:
+            if arg < result and low < arg < high:
+                result = arg
+        return result
 
     return inner
--- find_max.py
+++ find_max.py
@@ -1,29 +1,46 @@
 def get_max(a, b):
-    pass
+    return a if a > b else b
 
 
 def get_max_without_arguments():
-    pass
+    raise TypeError('no arg')
 
 
 def get_max_with_one_argument(a):
-    pass
+    return a
 
 
 def get_max_with_many_arguments(*args):
-    pass
+    inf = -100000
+    for a in args:
+        if a > inf:
+            inf = a
+    return inf
 
 
 def get_max_with_one_or_more_arguments(first, *args):
-    pass
+    for a in args:
+        if first < a:
+            first = a
+    return first
 
 
 def get_max_bounded(*args, low, high):
-    pass
+    inf = -100000000
+    for a in args:
+        if a > inf and low<a<high:
+            inf = a
+    return inf
 
 
 def make_max(*, low, high):
     def inner(first, *args):
-        pass
+        r = -100000
+        if low<first<high:
+            r = first
+        for a in args:
+            if r < a and low<a<high:
+                r = a
+        return r
 
     return inner
